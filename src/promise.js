b1257beefa001b450246e45dd604e4589a7877ff
maobao <maobao@duiba.com.cn>
2022-03-17 20:27:56 +0800

    feat: 增加执行函数的处理

--- src/promise.js
+++ src/promise.js
@@ -9,11 +9,23 @@
  */
 function noop () {}
 
+var LAST_ERROR = null
+var IS_ERROR = {}
+
+function tryCallTwo (fn, a, b) {
+  try {
+    return fn(a, b)
+  } catch (error) {
+    LAST_ERROR = error
+    return IS_ERROR
+  }
+}
+
 /**
  * Promise是一个构造函数
  * 传入的参数为一个可执行函数
  */
-function Promise (excutor) {
+function Promise (executor) {
   // 判断Promise的调用情况, 是否用new操作符来调用Promise
   if (typeof this !== 'object') {
     throw new TypeError('Promise must be constructed via new')
@@ -27,6 +39,46 @@ function Promise (excutor) {
 
   this._state = 0 // 初始化的状态
   this._value = null // 传递的值
+  // 调用函数
+  handleDoExecutor(executor, this)
+}
+
+/**
+ * 处理fulfilled状态
+ */
+function resolve (promise, value) {
+
+}
+
+/**
+ * 处理rejected状态
+ */
+function reject (promise, reason) {
+
+}
+
+/**
+ * 处理可执行函数
+ */
+function handleDoExecutor (fn, promise) {
+  // 定义一个执行结果的状态
+  var done = false
+  // 执行fn,传入reslove和reject两个参数
+  var promiseRes = tryCallTwo(fn, function (value) {
+    if (done) return
+    done = true
+    resolve(promise, value)
+  }, function (reason) {
+    if (done) return
+    done = true
+    reject(promise, reason)
+  })
+
+  // 函数内部执行出错，捕获错误后继续执行状态的传递和变更
+  if (!done && promiseRes === IS_ERROR) {
+    done = true
+    reject(promise, LAST_ERROR)
+  }
 }
 
 module.exports = Promise
